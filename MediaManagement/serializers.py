6d2a403fc589ce555b46ddf9617bca6046144d07
cediazz <dce92179@gmail.com>
2023-09-22 18:01:03 -0400

    InsertMediaContainer

--- MediaManagement/serializers.py
+++ MediaManagement/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Category,Plan,Coordinadas,Media,Field,MediaContainer,Media_Field
+from .models import Category, Plan, Coordinadas, Media, Field, MediaContainer, Media_Field
+
 
 class CategorySerializer(ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+
 
 class PlanSerializer(ModelSerializer):
     class Meta:
@@ -31,18 +33,28 @@ class FieldSerializer(ModelSerializer):
         model = Field
         fields = '__all__'
 
-class MediaSerializer(ModelSerializer):
 
+class MediaSerializer(ModelSerializer):
     coordinadas = CoordinadasSerializer()
+
     class Meta:
         model = Media
         fields = '__all__'
 
     def create(self, validated_data):
-        print(validated_data)
+        # Acceder a los datos no validados o enviados de más
+        extra_data = self.context['request'].data
+        media_father = None
+        if 'mediaFatherId' in extra_data:
+            media_father_id = extra_data['mediaFatherId']
+            media_father = Media.objects.filter(id = media_father_id).first()
         coordinada_data = validated_data.pop('coordinadas')
-        coordinadas = Coordinadas.objects.create(**coordinada_data)
-        media = Media.objects.create(coordinadas=coordinadas,**validated_data)
+        coordinadas = Coordinadas.objects.filter(lat=coordinada_data['lat'], lng=coordinada_data['lng']).first()
+        if coordinadas == None:
+            coordinadas = Coordinadas.objects.create(**coordinada_data)
+        media = Media.objects.create(coordinadas=coordinadas, **validated_data)
+        if(media_father != None):
+         MediaContainer.objects.create(father = media_father , son=media)
         return media
 
     def update(self, instance, validated_data):
@@ -53,17 +65,20 @@ class MediaSerializer(ModelSerializer):
                 coordinadas_serializer.save()
         return super().update(instance, validated_data)
 
+
 class MediaSerializerGet(ModelSerializer):
     coordinadas = CoordinadasSerializer(read_only=True)
     category = CategorySerializer(read_only=True)
     plan = PlanSerializer(read_only=True)
+
     class Meta:
         model = Media
         fields = '__all__'
 
-class Media_FieldSerializer(ModelSerializer):
 
+class Media_FieldSerializer(ModelSerializer):
     field = FieldSerializer()
+
     class Meta:
         model = Media_Field
         fields = '__all__'
@@ -72,48 +87,70 @@ class Media_FieldSerializer(ModelSerializer):
         print(validated_data)
         field_data = validated_data.pop('field')
         field = Field.objects.create(**field_data)
-        media_field = Media_Field.objects.create(field=field,**validated_data)
+        media_field = Media_Field.objects.create(field=field, **validated_data)
         return media_field
 
-class Media_FieldSerializerPost(ModelSerializer):
 
-   class Meta:
+class Media_FieldSerializerPost(ModelSerializer):
+    class Meta:
         model = Media_Field
         fields = '__all__'
+
 
 class Media_FieldSerializerGet(ModelSerializer):
     media = MediaSerializerGet(read_only=True)
     field = FieldSerializer(read_only=True)
+
     class Meta:
         model = Media_Field
         fields = '__all__'
 
-class FieldsAllSerializer(ModelSerializer):
 
+class FieldsAllSerializer(ModelSerializer):
     field = FieldSerializer(read_only=True)
+
     class Meta:
         model = Media_Field
         fields = ['field']
+
+
+
+
+
+class MediaContainerSerializer(ModelSerializer):
+
+     class Meta:
+        model = MediaContainer
+        fields = '__all__'
+
+
+class MediaContainerSonSerializer(ModelSerializer):
+    coordinadas = CoordinadasSerializer(read_only=True)
+    category = CategorySerializer(read_only=True)
+    plan = PlanSerializer(read_only=True)
+    media_fields = FieldsAllSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Media
+        fields = '__all__'
+
+class MediaContainerSerializerGet(ModelSerializer):
+
+    son = MediaContainerSonSerializer(read_only=True)
+
+
+    class Meta:
+        model = MediaContainer
+        fields = ['son']
+
 
 class Media_Fields_AllSerializer(ModelSerializer):
     coordinadas = CoordinadasSerializer(read_only=True)
     category = CategorySerializer(read_only=True)
     plan = PlanSerializer(read_only=True)
     media_fields = FieldsAllSerializer(many=True, read_only=True)
+    father_containers = MediaContainerSerializerGet(many= True,read_only=True)
+
     class Meta:
         model = Media
         fields = '__all__'
-
-class MediaContainerSerializer(ModelSerializer):
-    father = MediaSerializer()
-    son = MediaSerializer()
-    class Meta:
-        model = MediaContainer
-        fields = '__all__'
-
-
-
-
-
-
-
